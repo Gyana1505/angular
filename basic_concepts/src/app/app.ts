73f3c4c4975caf0220c8aba7432209dcec2642da
Gyanaranjan samal <165542618+Gyana1505@users.noreply.github.com>
2026-05-02 06:22:33 +0000

    .

--- basic_concepts/src/app/app.ts
+++ basic_concepts/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+<<<<<<< HEAD
     selector: 'app-root',
       imports: [RouterOutlet],
         templateUrl: './app.html',
@@ -10,3 +11,16 @@ import { RouterOutlet } from '@angular/router';
           export class App {
             protected readonly title = signal('Gyana');
             }
+=======
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('basic_concepts');
+  name="gyana"
+  x=5
+  y=10
+}
+>>>>>>> 1c7a7e8 (.)
